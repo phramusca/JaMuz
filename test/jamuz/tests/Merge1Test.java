8468fce5b0d1426debf0c409ffbbc138910082ca
phramusca <phramusca@gmail.com>
2017-03-06 00:26:42 +0100

    minor

--- test/jamuz/tests/Merge1Test.java
+++ test/jamuz/tests/Merge1Test.java
@@ -20,7 +20,6 @@ import jamuz.Jamuz;
 import jamuz.gui.PanelMain;
 import jamuz.ProcessHelper;
 import jamuz.Settings;
-import jamuz.process.check.PanelCheck;
 import java.io.File;
 import junit.framework.TestCase;
 import org.junit.Test;
@@ -40,41 +39,48 @@ public class Merge1Test extends TestCase {
     public void test() throws Exception {
         Settings.startGUI("Label.Check"); //Mandatory
  
-        //Create an album, 
+        /***********************************************************
+		 * Create an album
+		***********************************************************/
         AlbumBuffer.getAlbum("9e097b10-8160-491e-a310-e26e54a86a10", "MergeTest1_Creation").create();
         
-        //Scan library
+        /***********************************************************
+		 * Scan library quick
+		***********************************************************/
         ProcessHelper.scanLibraryQuick();
         AlbumBuffer.getAlbum("9e097b10-8160-491e-a310-e26e54a86a10", "MergeTest2_DB").checkDbAndFS(false);
         
+		/***********************************************************
+		 * Merge
+		***********************************************************/
         PanelMain.selectTab(Inter.get("Label.Merge"));
         ProcessHelper.merge();
-        
         AlbumBuffer.getAlbum("9e097b10-8160-491e-a310-e26e54a86a10", "MergeTest3").checkJaMuz();
         AlbumBuffer.getAlbum("9e097b10-8160-491e-a310-e26e54a86a10", "MergeTest3").checkStatSource(1, false, false);
         
 		//FIXME TEST Continue from here: Change some statistics
 		//Then use compareOO to double check and doc
 		
-        //Change stats in stat source
-        AlbumBuffer.getAlbum("9e097b10-8160-491e-a310-e26e54a86a10", "MergeTest4_1").setAndCheckStatsInStatSource(1, false); // Guayadeque
-        //Change stats in JamuZ
+		/***********************************************************
+		 * Change stats in stat source & JamuZ
+		***********************************************************/
+        AlbumBuffer.getAlbum("9e097b10-8160-491e-a310-e26e54a86a10", "MergeTest4_1").setAndCheckStatsInStatSource(1, false); // Guayadeque 
         AlbumBuffer.getAlbum("9e097b10-8160-491e-a310-e26e54a86a10", "MergeTest4_JaMuz").setAndCheckStatsInJamuzDb();
 
-        //Merge again and check merge ok
+		/***********************************************************
+		 * Merge again and check merge ok
+		***********************************************************/
         ProcessHelper.merge();
         AlbumBuffer.getAlbum("9e097b10-8160-491e-a310-e26e54a86a10", "MergeTest4_New").checkJaMuz();
         AlbumBuffer.getAlbum("9e097b10-8160-491e-a310-e26e54a86a10", "MergeTest4_New").checkStatSource(1, false, false);
         
 		//FIXME TEST ADD some more testing on playCounter especially
-		
+		/***********************************************************
+		 * REAPLCE
+		***********************************************************/
 		
         assertTrue("Not valid test. Shall no pass yet !", false);
 
-    }
-
-    private void checkNumberScanned(int expected){
-        assertEquals("number of checked folders", expected, PanelCheck.tableModelActionQueue.getFolders().size());
     }
 
 	/**
