eb43e1f8d7409283fe8579f1e7bcfc8b4d0ec831
phramusca <raphael.camus@free.fr>
2018-03-17 23:52:25 +0100

    test

--- src/jamuz/process/video/DialogVideoOption.java
+++ src/jamuz/process/video/DialogVideoOption.java
@@ -437,16 +437,13 @@ public class DialogVideoOption extends javax.swing.JDialog {
         //</editor-fold>
 
         /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                DialogVideoOption dialog = new DialogVideoOption(new javax.swing.JFrame(), true);
-                //Center the dialog on screen
-                dialog.setLocationRelativeTo(dialog.getParent());
-                //Display
-                dialog.setVisible(true);
-            }
-        });
+        java.awt.EventQueue.invokeLater(() -> {
+			DialogVideoOption dialog = new DialogVideoOption(new javax.swing.JFrame(), true);
+			//Center the dialog on screen
+			dialog.setLocationRelativeTo(dialog.getParent());
+			//Display
+			dialog.setVisible(true);
+		});
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
