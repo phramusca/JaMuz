bf438e8c90d1f6d8efb85932ab4dd9ff7601900d
Unknown <raphael.camus@free.fr>
2018-07-14 00:31:47 +0200

    minor

--- src/jamuz/remote/PanelRemote.java
+++ src/jamuz/remote/PanelRemote.java
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package jamuz.remote;
+
 import jamuz.FileInfo;
 import jamuz.FileInfoInt;
 import jamuz.gui.DialogQRcode;
