391156cbad4d41342a997cfc1a849491257a6705
phramusca <phramusca@gmail.com>
2020-08-15 01:57:44 +0200

    refactor  part II

--- src/jamuz/acoustid/AcoustID.java
+++ src/jamuz/acoustid/AcoustID.java
@@ -20,15 +20,10 @@ import java.util.logging.Logger;
  */
 public class AcoustID {
 	
-	public static AcoustIdResult analyze(String filename, String key) {
+	public static Results analyze(String filename, String key) {
 		try {	
 			ChromaPrint chromaprint = chromaprint(new File(filename), "fpcalc");		
-			Result result = lookup(chromaprint, key);
-			if(result!=null) {
-				AcoustIdResult acoustIdResult = result.getMeta();
-				acoustIdResult.setFilename(filename);
-				return acoustIdResult;
-			}
+			return lookup(chromaprint, key);
 		} catch (IOException ex) {
 			Logger.getLogger(AcoustID.class.getName()).log(Level.SEVERE, null, ex);
 		}
@@ -42,33 +37,33 @@ public class AcoustID {
 	 * @return 
 	 * @throws java.io.IOException
     */
-   public static ChromaPrint chromaprint(File file, String fpcalc) throws IOException {
-      final ProcessBuilder processBuilder = new ProcessBuilder(fpcalc, null);
-      processBuilder.redirectErrorStream(true);
-      processBuilder.command().set(1, file.getAbsolutePath());
-      final Process fpcalcProc = processBuilder.start();
-      final BufferedReader br = new BufferedReader(new InputStreamReader(fpcalcProc.getInputStream()));
-      String line;
-      String chromaprint = null;
-      String duration = null;
-      while ((line = br.readLine()) != null) {
-         if (line.startsWith("FINGERPRINT=")) {
-            chromaprint = line.substring("FINGERPRINT=".length());
-         } else if (line.startsWith("DURATION=")) {
-            duration = line.substring("DURATION=".length());
-         }
-      }
-      return new ChromaPrint(chromaprint, duration);
-   }
+	public static ChromaPrint chromaprint(File file, String fpcalc) throws IOException {
+		final ProcessBuilder processBuilder = new ProcessBuilder(fpcalc, null);
+		processBuilder.redirectErrorStream(true);
+		processBuilder.command().set(1, file.getAbsolutePath());
+		final Process fpcalcProc = processBuilder.start();
+		final BufferedReader br = new BufferedReader(new InputStreamReader(fpcalcProc.getInputStream()));
+		String line;
+		String chromaprint = null;
+		String duration = null;
+		while ((line = br.readLine()) != null) {
+			if (line.startsWith("FINGERPRINT=")) {
+				chromaprint = line.substring("FINGERPRINT=".length());
+			} else if (line.startsWith("DURATION=")) {
+				duration = line.substring("DURATION=".length());
+			}
+		}
+		return new ChromaPrint(chromaprint, duration);
+	}
 
-   /**
-    * do a ChromaPrint lookup and result a musicbrainz id
+	/**
+	 * do a ChromaPrint lookup
 	 * @param chromaprint
 	 * @param key
 	 * @return 
 	 * @throws java.io.IOException
     */
-	public static Result lookup(ChromaPrint chromaprint, String key) throws IOException {
+	public static Results lookup(ChromaPrint chromaprint, String key) throws IOException {
 		OkHttpClient client = new OkHttpClient();		
 		HttpUrl.Builder urlBuilder = HttpUrl.parse("http://api.acoustid.org/v2/lookup").newBuilder();
 		urlBuilder.addQueryParameter("client", key);
@@ -78,12 +73,7 @@ public class AcoustID {
 		String url = urlBuilder.build().toString();
 		Request request = new Request.Builder().url(url).build();
 		Response response = client.newCall(request).execute();
-
 		final Gson gson = new Gson();
-		final Results results = gson.fromJson(response.body().string(), Results.class);
-		if (results.status.compareTo("ok") == 0) {
-		   return results.results.stream().max(Comparator.comparing(Result::getScore)).get();
-		}
-		return null;
+		return gson.fromJson(response.body().string(), Results.class);
 	}
 }
