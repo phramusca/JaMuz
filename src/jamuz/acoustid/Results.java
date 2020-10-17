7ead6076c0e5e5abceb1a2dfcf49237d3fb81ca3
phramusca <raphael.camus@free.fr>
2020-10-17 19:35:45 +0200

    AcoustId

--- src/jamuz/acoustid/Results.java
+++ src/jamuz/acoustid/Results.java
@@ -3,18 +3,25 @@ package jamuz.acoustid;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Results {
 	String status;
 	List<Result> results = new ArrayList<>();
+	AcoustIdResult bestResult;
 	ChromaPrint chromaprint;
-   
+	
 	public AcoustIdResult getBest() {
-		if (status.compareTo("ok") == 0) {
-			Result best = results.stream().max(Comparator.comparing(Result::getScore)).get();
-			return best.getFirst();
+		if(bestResult==null) {
+			if (status.compareTo("ok") == 0 && results.size()>0) {
+				Result best = results.stream().max(Comparator.comparing(Result::getScore)).get();
+				try {
+					bestResult = best.getFirst();
+				} catch(NoSuchElementException ex) {
+				}
+			}	
 		}
-		return null;
+		return bestResult;
 	}
 
 	public ChromaPrint getChromaprint() {
