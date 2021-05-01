b0afd81b21ecd635084140040f0a9c777e8bc097
phramusca <raphael.camus@free.fr>
2021-05-01 15:21:15 +0200

    fix merge

--- src/jamuz/remote/Server.java
+++ src/jamuz/remote/Server.java
@@ -229,13 +229,13 @@ public class Server {
 				+ " AND DF.idDevice="+device.getId()+" AND DF.status=\""+DbConnJaMuz.SyncStatus.NEW+"\""
 				+ " ORDER BY F.idFile "
 				+ limit;
-				setStatus(login, "Sending "+(getCount?"count":"list") + " of NEW files");
+				setStatus(login, "Sending "+(getCount?"count":"list") + " of NEW files ("+limit+")");
 				if(getCount) {
 					res.send(Jamuz.getDb().getFilesCount(sql).toString());
 				} else {
 					res.send(getFiles(sql));
 				}
-				setStatus(login, "Sent "+(getCount?"count":"list") + " of NEW files");
+				setStatus(login, "Sent "+(getCount?"count":"list") + " of NEW files ("+limit+")");
 			});
 			
 			app.get("/files/info", (req, res) -> {
@@ -258,13 +258,13 @@ public class Server {
 				+ " AND F.idFile NOT IN (SELECT idFile FROM deviceFile WHERE idDevice="+device.getId()+" AND status=\""+DbConnJaMuz.SyncStatus.NEW+"\") "
 				+ " ORDER BY F.idFile "
 				+ limit;
-				setStatus(login, "Sending "+(getCount?"count":"list") + " of INFO files");
+				setStatus(login, "Sending "+(getCount?"count":"list") + " of INFO files ("+limit+")");
 				if(getCount) {
 					res.send(Jamuz.getDb().getFilesCount(sql).toString());
 				} else {
 					res.send(getFiles(sql));
 				}
-				setStatus(login, "Sending "+(getCount?"count":"list") + " of INFO files");
+				setStatus(login, "Sending "+(getCount?"count":"list") + " of INFO files ("+limit+")");
 			});
 			
 			app.listen(port+1); // port is already used by remote
