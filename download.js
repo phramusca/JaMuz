87e6407b891d1fb12f67bec066f345997598aa30
phramusca <phramusca@gmail.com>
2023-11-27 23:50:07 +0100

    fix?

--- download.js
+++ download.js
@@ -1,4 +1,3 @@
-// Add this to your download.js file
 document.addEventListener("DOMContentLoaded", async function () {
     const repositories = [
         { repoName: "phramusca/JaMuz", containerId: "download-container-jamuz-desktop" },
@@ -13,38 +12,18 @@ document.addEventListener("DOMContentLoaded", async function () {
         const downloadUrl = `https://github.com/${repo.repoName}/releases/latest/download/${assetName}`;
         const version = data.tag_name;
 
-        // Create an invisible link
         const link = document.createElement('a');
         link.href = downloadUrl;
         link.download = assetName;
-        link.textContent = `Download latest version ${version}`;
+        link.textContent = `Download latest version ${version} (${repo.repoName})`;
 
         // Specify the location on the page to append the link
         const downloadContainer = document.getElementById(repo.containerId);
         if (downloadContainer) {
             downloadContainer.appendChild(link);
         } else {
+            // If the specified container doesn't exist, append the link to the body
             document.body.appendChild(link);
         }
     }
 });
-
-// async function getLatestRelease(repoName) {
-//     const apiUrl = `https://api.github.com/repos/${repoName}/releases/latest`;
-//     const response = await fetch(apiUrl);
-//     const data = await response.json();
-//     const assetName = data.assets[0].name;
-//     const downloadUrl = `https://github.com/${repoName}/releases/latest/download/${assetName}`;
-    
-//     // Create an invisible link
-//     const link = document.createElement('a');
-//     link.href = downloadUrl;
-//     link.download = assetName;
-
-//     // Append the link to the document and trigger a click
-//     document.body.appendChild(link);
-//     link.click();
-
-//     // Remove the link from the document
-//     document.body.removeChild(link);
-// }
