63f80f00a9d4a8e32873f916c5f6bb56a72502f5
phramusca <phramusca@gmail.com>
2023-11-28 01:10:14 +0100

    main page

--- download.js
+++ download.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", async function () {
     const repositories = [
-        { repoName: "phramusca/JaMuz", containerId: "download-container-jamuz-desktop" },
-        { repoName: "phramusca/JaMuz-Remote", containerId: "download-container-jamuz-android" }
+        { Url: "phramusca/JaMuz", containerId: "download-container-jamuz-desktop" },
+        { Url: "phramusca/JaMuz-Remote", containerId: "download-container-jamuz-android" }
     ];
 
     for (const repo of repositories) {
-        const apiUrl = `https://api.github.com/repos/${repo.repoName}/releases/latest`;
+        const apiUrl = `https://api.github.com/repos/${repo.Url}/releases/latest`;
         const response = await fetch(apiUrl);
         const data = await response.json();
         const assetName = data.assets[0].name;
-        const downloadUrl = `https://github.com/${repo.repoName}/releases/latest/download/${assetName}`;
+        const downloadUrl = `https://github.com/${repo.Url}/releases/latest/download/${assetName}`;
         const version = data.tag_name;
 
         const link = document.createElement('a');
         link.href = downloadUrl;
         link.download = assetName;
-        link.textContent = `Download latest version ${version} (${repo.repoName})`;
+        link.textContent = `Download latest version (${assetName})`;
 
         // Specify the location on the page to append the link
         const downloadContainer = document.getElementById(repo.containerId);
