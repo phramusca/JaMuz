c29fc0ea5a6f387ebc6d7b2f96324fb10b9233f6
phramusca <phramusca@gmail.com>
2023-12-22 21:13:56 +0100

    init testClient & fix inter bundle in tests

--- src/test/java/jamuz/database/DaoClientTest.java
+++ src/test/java/jamuz/database/DaoClientTest.java
@@ -16,7 +16,13 @@
  */
 package jamuz.database;
 
+import jamuz.Jamuz;
+import jamuz.Playlist;
+import jamuz.process.merge.StatSource;
+import jamuz.process.sync.Device;
 import jamuz.remote.ClientInfo;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -24,74 +30,126 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
+import test.helpers.TestUnitSettings;
 
 /**
  *
  * @author phramusca <phramusca@gmail.com>
  */
 public class DaoClientTest {
-	
-	public DaoClientTest() {
-	}
-	
-	@BeforeClass
-	public static void setUpClass() {
-	}
-	
-	@AfterClass
-	public static void tearDownClass() {
-	}
-	
-	@Before
-	public void setUp() {
-	}
-	
-	@After
-	public void tearDown() {
-	}
 
-	/**
-	 * Test of lock method, of class DaoClient.
-	 */
-	@Test
-	public void testLock() {
-		System.out.println("lock");
-		DaoClient instance = null;
-		DaoClientWrite expResult = null;
-		DaoClientWrite result = instance.lock();
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
+    private static DbConnJaMuz dbConnJaMuz;
 
-	/**
-	 * Test of get method, of class DaoClient.
-	 */
-	@Test
-	public void testGet_String() {
-		System.out.println("get");
-		String login = "";
-		DaoClient instance = null;
-		ClientInfo expResult = null;
-		ClientInfo result = instance.get(login);
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
+    public DaoClientTest() {
+    }
 
-	/**
-	 * Test of get method, of class DaoClient.
-	 */
-	@Test
-	public void testGet_LinkedHashMap() {
-		System.out.println("get");
-		LinkedHashMap<Integer, ClientInfo> clients = null;
-		DaoClient instance = null;
-		boolean expResult = false;
-		boolean result = instance.get(clients);
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-	
+    @BeforeClass
+    public static void setUpClass() throws SQLException, ClassNotFoundException, IOException {
+        dbConnJaMuz = TestUnitSettings.createTempDatabase();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        TestUnitSettings.cleanupTempDatabase(dbConnJaMuz);
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testClient() {
+
+        System.out.println("testClient");
+        String hostname = "testClientMachine";
+        dbConnJaMuz.machine().lock().getOrInsert(hostname, new StringBuilder(), true);
+        Playlist playlist = new Playlist(0, "whateverName",false, 0, Playlist.LimitUnit.Gio, false, Playlist.Type.Albums, Playlist.Match.All, false, "exttt");
+        dbConnJaMuz.playlist().lock().insert(playlist);
+        Device device = new Device(-1, "login", "source", "clientLogin", 1, hostname, true);
+        device.setIdPlaylist(1);
+        dbConnJaMuz.device().lock().insertOrUpdate(device);
+        StatSource statSource = new StatSource(hostname);
+        dbConnJaMuz.statSource().lock().insertOrUpdate(statSource);
+        ClientInfo clientInfo = new ClientInfo("login", "pwd", "rootPath", "name", true);
+        clientInfo.setDevice(new Device(1, hostname, "", "", 1, hostname, true));
+        statSource.setId(1);
+        clientInfo.setStatSource(statSource);
+        Jamuz.setDb(dbConnJaMuz);
+        Jamuz.readPlaylists(); //in case of an error in below method
+        
+        //When
+        dbConnJaMuz.client().lock().insertOrUpdate(clientInfo);
+
+        //Then
+        //FIXME TEST: Assert
+        
+        // FIXME TEST Check all those methods
+        
+        
+        clientInfo.setConnected(true);
+
+        clientInfo = new ClientInfo(1, clientInfo.getLogin(), clientInfo.getName(), clientInfo.getPwd(), clientInfo.getDevice(), clientInfo.getStatSource(), true);
+        dbConnJaMuz.client().lock().insertOrUpdate(clientInfo);
+
+        dbConnJaMuz.client().get("login");
+
+        LinkedHashMap<Integer, ClientInfo> clients = new LinkedHashMap<>();
+
+        dbConnJaMuz.client().get(clients);
+
+        // FIXME TEST Check other constraints
+    }
+
+    /**
+     * Test of lock method, of class DaoClient.
+     */
+    @Test
+    @Ignore
+    public void testLock() {
+        System.out.println("lock");
+        DaoClient instance = null;
+        DaoClientWrite expResult = null;
+        DaoClientWrite result = instance.lock();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of get method, of class DaoClient.
+     */
+    @Test
+    @Ignore
+    public void testGet_String() {
+        System.out.println("get");
+        String login = "";
+        DaoClient instance = null;
+        ClientInfo expResult = null;
+        ClientInfo result = instance.get(login);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of get method, of class DaoClient.
+     */
+    @Test
+    @Ignore
+    public void testGet_LinkedHashMap() {
+        System.out.println("get");
+        LinkedHashMap<Integer, ClientInfo> clients = null;
+        DaoClient instance = null;
+        boolean expResult = false;
+        boolean result = instance.get(clients);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
 }
